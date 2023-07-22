499a7e5a5dc4ec36be7a75ca46be46859d226d20
mjmj2277 <131595280+mjmj2277@users.noreply.github.com>
2023-07-22 21:56:15 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(Math.sqrt(n) == (int)Math.sqrt(n)){
+            answer = 1;
+        }else{
+            answer = 2;
+        }
+        return answer;
+    }
+}
