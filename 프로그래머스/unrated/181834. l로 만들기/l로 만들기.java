1a8eebc9f2ac776ab7e6508480cf07eafa4284db
mjmj2277 <131595280+mjmj2277@users.noreply.github.com>
2023-07-25 16:31:37 +0900

    [unrated] Title: l로 만들기, Time: 19.00 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,16 +1,16 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        String[] arr_i = {"a","b","c","d","e","f","g","h","i","j","k"};
-        String[] arr = myString.split("");
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr_i.length; j++){
-                if(arr[i].equals(arr_i[j])){
-                    arr[i] = "l";
-                }   
-            }
-            answer += arr[i];
-        }
+        String answer = myString.replaceAll("[a-k]","l");
+        // String[] arr_i = {"a","b","c","d","e","f","g","h","i","j","k"};
+        // String[] arr = myString.split("");
+        // for(int i = 0; i < arr.length; i++){
+        //     for(int j = 0; j < arr_i.length; j++){
+        //         if(arr[i].equals(arr_i[j])){
+        //             arr[i] = "l";
+        //         }   
+        //     }
+        //     answer += arr[i];
+        // }
         return answer;
     }
 }
