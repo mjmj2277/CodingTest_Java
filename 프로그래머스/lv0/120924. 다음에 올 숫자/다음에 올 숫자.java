279f8fb7db0fcf80c3c379034723c6c4f8dd28bd
mjmj2277 <131595280+mjmj2277@users.noreply.github.com>
2023-07-30 16:58:53 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        int a = common[0];
+        int b = common[1];
+        int c = common[2];
+            
+        if(b - a == c - b){
+            answer = common[common.length-1] + (b - a);
+        }else{
+            answer = common[common.length-1] * (b / a);
+        }
+        
+        return answer;
+    }
+}
