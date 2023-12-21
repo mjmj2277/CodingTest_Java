0ec9cb52e051f36ab683f3e618bb57f4b12f57d9
Minji Kim <131595280+mjmj2277@users.noreply.github.com>
2023-12-21 17:46:05 +0900

    Update 공 던지기.java

--- 프로그래머스/unrated/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/unrated/120843. 공 던지기/공 던지기.java
@@ -1,11 +1,8 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 0;
-        int idx = 0;
-        idx = idx  + ((k-1) * 2);
-        idx = idx % numbers.length;
-        answer = numbers[idx];        
-        return answer;
+        int index = (k - 1) * 2 % numbers.length;
+        return numbers[index];
     }
 }
